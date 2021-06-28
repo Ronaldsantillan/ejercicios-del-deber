b3fb3cad002ccda17904d20ba9ca16815261f217
Ronaldsantillan <86032264+Ronaldsantillan@users.noreply.github.com>
2021-06-27 21:01:54 -0500

    Add files via upload

---
+++ ejemplo10.py
@@ -0,0 +1,17 @@
+# Una escuela aplica dos exámenes a sus aspirantes, por lo que cada uno de ellos obtiene dos calificaciones
+# denotadas como C1 y C2. El aspirante que obtenga una calificación mayor
+# que 90 en cualquiera de los exámenes es aceptado; en caso contrario es rechazado.
+
+
+class operador:
+    c1=float(input("ingrse calificacion1: "))
+    c2=float(input("ingrse calificacion2: "))
+    if (c1>=90) or (c2>=90):
+           print("usted ha sido aceptado")
+    else:
+          print("usted ha sido rechazado")
+          
+Operador=operador()         
+
+
+    
